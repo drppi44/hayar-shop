77885f892f860b728226817c6bd689b81e66d833
drppi44 <multinigel@gmail.com>
2015-02-15 10:15:01 +0200

    Second commit

---
+++ testapp/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from models import *
+
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display=['name','description','get_category','subcategory','price','date']
+	def get_category(self,obj):
+		return obj.subcategory.category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Param)
+class ParamAdmin(admin.ModelAdmin):
+	list_display=['name','value','product']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+	list_display=['first_name','second_name']
